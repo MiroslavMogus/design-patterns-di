427dc3487a20e52f8784046b0e90a9672224aad8
Miroslav Moguš <miromogus@gmail.com>
2019-05-29 14:50:22 +0200

    Initial commit

---
+++ src/test/java/miro/di/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package miro.di.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
